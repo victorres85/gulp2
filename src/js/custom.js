2708838d0dbbd4eecde534adc98bd0d64cdd2eb6
Victo Almeida <victorres.almeida@gmail.com>
2022-04-07 11:32:37 +0100

    inclusao de modal pop ups

--- src/js/custom.js
+++ src/js/custom.js
@@ -13,3 +13,50 @@ $(document).ready(function () {
 
 });
 
+var alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+var alertTrigger = document.getElementById('liveAlertBtn')
+var confirmation = document.getElementById('confirmationBtn')
+var noConfirmation = document.getElementById('noConfirmationBtn')
+var confirmation = document.getElementById('confirmationBtn')
+var noConfirmation = document.getElementById('noConfirmationBtn')
+
+
+function alert(message, type) {
+    var wrapper = document.createElement('div')
+    wrapper.innerHTML = '<div class="alert alert-' + type + ' alert-dismissible" role="alert">' + message + '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>'
+
+    alertPlaceholder.append(wrapper)
+}
+
+if (alertTrigger) {
+    alertTrigger.addEventListener('click', function () {
+        alert('Obrigado por adicionar seu contato, te avisaremos assim que nosso estoque voltar!', 'success')
+    })
+
+}
+if (confirmation) {
+    confirmation.addEventListener('click', function () {
+        alert('O vinho foi adicionado ao seu carrinho', 'success')
+    })
+
+}
+if (noConfirmation) {
+    noConfirmation.addEventListener('click', function () {
+        alert('O vinho não foi adicionado ao seu carrinho', 'danger')
+    })
+
+}
+if (confirmation2) {
+    confirmation2.addEventListener('click', function () {
+        alert('O vinho foi adicionado ao seu carrinho', 'success')
+    })
+
+}
+if (noConfirmation2) {
+    noConfirmation2.addEventListener('click', function () {
+        alert('O vinho não foi adicionado ao seu carrinho', 'danger')
+    })
+
+}
+
+
