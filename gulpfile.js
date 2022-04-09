66532a84df4915ff6ed65ade1a40ebf24e5dc6a6
Victo Almeida <victorres.almeida@gmail.com>
2022-04-09 22:56:46 +0100

    final de exercicio modulo 18

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ const rename = require('gulp-rename')
 const cssmin = require('gulp-cssmin')
 const uglify = require('gulp-uglify')
 const image = require('gulp-imagemin')
+const stripCSS = require('gulp-strip-css-comments')
 
 
 function tarefasImagem() {
@@ -23,22 +24,36 @@ function tarefasImagem() {
         .pipe(gulp.dest('./dist/images'))
 }
 
-function tarefaCSS(cb) {
-    return gulp.src('./src/vendor/**/*.css')
-        .pipe(concat('libs.css'))
-        .pipe(cssmin())
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(gulp.dest('./dist'))
+function tarefasCSS(cb) {
+
+    return gulp.src([
+        './node_modules/bootstrap/dist/css/bootstrap.css',
+        './node_modules/@fortawesome/fontawesome-free/css/brands.css',
+        './src/vendor/owl/css/owl.css',
+        './src/vendor/jquery-ui/jquery-ui.css',
+        './src/css/custom.css'])
+        .pipe(stripCSS())                   // remove comentários
+        .pipe(concat('styles.css'))         // mescla arquivos
+        .pipe(cssmin())                     // minifica css
+        .pipe(rename({ suffix: '.min' }))    // styles.min.css
+        .pipe(gulp.dest('./dist/css'))      // cria arquivo em novo diretório
+
 }
 
 function tarefaJS() {
-    return gulp.src('./src/vendor/**/*.js')
+    return gulp.src([
+        './node_modules/jquery/dist/jquery.js',
+        './node_modules/bootstrap/dist/js/bootstrap.js',
+        './src/vendor/owl/js/owl.js',
+        './src/vendor/jquery-mask/jquery.mask.js',
+        './src/vendor/jquery-ui/jquery-ui.js',
+        './src/js/custom.js'])
         .pipe(concat('libs.js'))
         .pipe(uglify())
         .pipe(rename({ suffix: '.min' }))
         .pipe(gulp.dest('./dist/js'))
 }
 
-exports.styles = tarefaCSS
+exports.styles = tarefasCSS
 exports.scripts = tarefaJS
 exports.images = tarefasImagem
